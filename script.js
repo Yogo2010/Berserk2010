74929a72055c258df51ffb5ae36e208bfe69712a
Yogo2010 <karpenko.a16012010@gmail.com>
2025-03-29 21:48:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById("videoModal").style.display = "flex";
+}
+
+function closeModal() {
+    document.getElementById("videoModal").style.display = "none";
+}
